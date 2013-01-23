855abdf7edd85954a64fd2343c8218886af798b4
Andy M <andy.dude@gmail.com>
2013-01-22 21:04:58 -0600

    Fixed some issues, added readme, also made actual animation.

--- interactiveText.js
+++ interactiveText.js
@@ -1,18 +1,18 @@
 interactiveText = new function () {
 
     this.clickEventList = [];
-    
+
     var hiddenContainer = null;
 
     function init() {
-      
+
         // add a div to find the length/height in pixels for the text
         $('body').append('<span class="interactiveText" style="visibility: hidden;display: inline-block; white-space: nowrap;"></span>')
         hiddenContainer = $('.interactiveText');
     }
 
 
-  
+
 
     function addTextToCanvas(context, textObject) {
 
@@ -23,6 +23,9 @@ interactiveText = new function () {
         // 4. store a "block" around the text so I know whereabouts the text shoudl be. 
         // 5. need a quick way of checking to see if the item is being interacted with.
 
+        if (textObject.fillstyle) {
+            context.fillStyle = textObject.fillstyle;
+        }
         if (textObject.font)
             hiddenContainer.css('font', textObject.font);
 
@@ -33,10 +36,15 @@ interactiveText = new function () {
 
         // set the hight and width of the rectangle incase something has changed
         textObject.x1 = actualWidth + textObject.x0;
-        textObject.y1 = actualHeight + textObject.y1;
+        textObject.y1 = actualHeight + textObject.y0;
+
+        console.log("x0: " + textObject.x0 + "; x1: " + textObject.x1 +
+            "; y0: " + textObject.y0 + "; y1: " + textObject.y1);
 
         if (textObject.font)
             context.font = textObject.font;
+
+
         context.fillText(textObject.text, textObject.x0, textObject.y1);
     }
 
@@ -47,7 +55,7 @@ interactiveText = new function () {
         }
     }
 
-    
+
     this.addTextToCanvas = function (context, text, options) {
 
         var xStart = options["x"];
@@ -55,10 +63,8 @@ interactiveText = new function () {
         var fillStyle = options['fillstyle'];
         var font = options['font'];
         var clickEvent = options['onclick'];
-        if (fillStyle) {
-            context.fillStyle = fillStyle;
-        }
-        
+
+
 
         if (clickEvent) {
             // if first click event added then add the generic event to the canvas
@@ -82,8 +88,8 @@ interactiveText = new function () {
             // add event to the list
             // change the Y, based on position of the text... assumes normal right now.
             this.clickEventList.push({
-                'x0': xStart, 'x1':xStart, 'y0': yStart, 'y1': yStart,
-                'text': text, 'event': clickEvent, 'font': font
+                'x0': xStart, 'x1': xStart, 'y0': yStart, 'y1': yStart,
+                'text': text, 'event': clickEvent, 'font': font, 'fillstyle': fillStyle
             });
 
         }
